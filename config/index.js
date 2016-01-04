f59ad676e9336de49eaec57cedf06c3a62b5dc09
Matt Garnes <matt@mattgarnes.com>
2016-01-03 21:36:47 -0500

    Set date of event to correct date

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ config = {
         emails  : path.resolve('views/emails/')
     },
 
-    date: new Date('Sat Oct 5 2013 4:30:00 GMT-0400 (EDT)'),
+    date: new Date('Sat Aug 27 2016 1:30:00 GMT-0700 (PDT)'),
 
     version: require('../package').version,
 
